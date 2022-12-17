2735fb96d5f7bab3829de8aba5c7c1df27f758c8
Paul Delifer <paul.delifer@yahoo.fr>
2022-12-17 15:33:10 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls} from "@react-three/drei";
+
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 function App() {
+    function Scene() {
+        const gltf = useLoader(GLTFLoader, '/original_backrooms/scene.gltf')
+        return <primitive object={gltf.scene} />
+                         }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+        <Scene />
+        <OrbitControls />
+    </Canvas>
+      // <Canvas>
+      //     <ambientLight intensity={0.5} />
+      //     <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      //     <pointLight position={[-10, -10, -10]} />
+      //     <Box position={[-1.2, 0, 0]} />
+      //     <Box position={[1.2, 0, 0]} />
+      //     <OrbitControls />
+      // </Canvas>
   );
 }
 
